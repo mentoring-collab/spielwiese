a4f82dc0c64ab3aa83aacf7e8c1f7a8a4184af7e
Beatriz Bodas Rodriguez <beatriz.bodas.rodriguez@msg.group>
2022-11-30 18:38:59 +0100

    SupplierTest erweitert

--- lambdas/SupplierTest.java
+++ lambdas/SupplierTest.java
@@ -1,7 +1,9 @@
 package lambdas;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.function.*;
 
 public class SupplierTest {
     public static void main(String[] args) {
@@ -14,6 +16,23 @@ public class SupplierTest {
         project.add("vdr");
 
         System.out.println(project.get(1));
-    }
 
+        String client = "tc";
+        double price = 659.50;
+
+        BooleanSupplier boolSupplier = () -> client.length() == 10;
+        IntSupplier intSupplier = () -> client.length() - 2;
+        DoubleSupplier doubleSupplier = () -> price -20;
+        LongSupplier longSupplier = () -> new Date().getTime();
+        Supplier<String> supplier = () -> client.toUpperCase();
+        Supplier<String> supplierLow = () -> client.toLowerCase();
+
+
+        System.out.println(boolSupplier.getAsBoolean());
+        System.out.println(intSupplier.getAsInt());
+        System.out.println(doubleSupplier.getAsDouble());
+        System.out.println(longSupplier.getAsLong());
+        System.out.println(supplier.get());
+        System.out.println(supplierLow.get());
+    }
 }
